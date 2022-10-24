3018009e64bb90c98f8ce51b8e695b98985f8ed4
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-10-24 14:56:03 +0200

    opg 3 clear

--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ console.log(myElement);
 
 let myList = document.getElementsByTagName("li");
 console.log(myList);
+
+myList = document.getElementsByClassName("healthy");
+console.log(myList);
+
+for (let x of myList) {
+    console.log(x);
+}
+
+myElement = document.getElementById("3");
+myElement.setAttribute("class", "unhealthy");
+myElement.setAttribute("class", "healthy")
+myElement.setAttribute("hidden", "true");
+myElement.removeAttribute("hidden");
+
+myList = document.getElementsByClassName("unhealthy");
+
+for (let index = 0; myList.length > 0; ) {
+    myList[index].setAttribute("class", "healthy");
+    
+}
+
+
+
