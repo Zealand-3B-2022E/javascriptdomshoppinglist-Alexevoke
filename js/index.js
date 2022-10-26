25e0e7649cb4474e20ed415058a4de6e82805771
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-10-26 12:34:54 +0200

    moved unhealthy to the other list

--- js/index.js
+++ js/index.js
@@ -23,9 +23,48 @@ myElement.removeAttribute("hidden");
 myList = document.getElementsByClassName("unhealthy");
 
 for (let index = 0; myList.length > 0; ) {
-    myList[index].setAttribute("class", "healthy");
-    
+    myList[index].setAttribute("class", "healthy");  
 }
 
+myElement = document.getElementById("0")
+myElement.setAttribute("class", "unhealthy")
+myElement = document.getElementById("2")
+myElement.setAttribute("class", "unhealthy")
 
+
+myElement = document.getElementById("1").parentNode
+
+myElement.firstElementChild.setAttribute("class", "healthy")
+myElement.lastElementChild.setAttribute("class", "unhealthy")
+
+
+const node = document.createElement("li")
+node.setAttribute("id", "4")
+node.setAttribute("class", "healthy")
+const textnode = document.createTextNode("Banan")
+node.appendChild(textnode)
+myElement.appendChild(node)
+
+myElement = document.getElementById("0")
+myElement.setAttribute("class", "unhealthy")
+myElement = document.getElementById("3")
+myElement.setAttribute("class", "healthy")
+
+myElement = document.getElementById("list")
+
+const node2 = document.createElement("li")
+node2.setAttribute("id", "5")
+node2.setAttribute("class", "unhealthy")
+const textnode2 = document.createTextNode("Cola")
+node2.appendChild(textnode2)
+
+myElement.insertBefore(node2, myElement.children[3])
+
+const myList1 = document.getElementsByClassName("unhealthy")
+const myList2 = document.getElementById("listUnhealthy")
+
+for (let index = myList1.length - 1; index >= 0; index--) {
+    const element = myList1[index];
+    myList2.insertBefore(element, myList2.children[0])
+}
 
